69de3a9e250de1b425a21c5a49f511bf10bf1650
Vitor-HenriqueAS <vitorhenrique.a12@gmail.com>
2024-01-14 14:14:09 -0300

    atualizando enderecos dos projetos

--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -5,8 +5,8 @@ const projects = [
     "date":"30/08/2022",
     "level":"newbie",
     "languages": "html / css / js",
-    "deploy":"pages/interactive-rating-component/index.html",
-    "code":"",
+    "deploy":"pages/interactive-rating/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/interactive-rating",
   },
   {
     "image":"order-summary",
@@ -14,8 +14,8 @@ const projects = [
     "date":"16/08/2022",
     "level":"newbie",
     "languages": "html / css",
-    "deploy":"pages/order-summary-component/index.html",
-    "code":"",
+    "deploy":"pages/order-summary/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/order-summary",
   },
   {
     "image":"3-column",
@@ -23,8 +23,8 @@ const projects = [
     "date":"15/08/2022",
     "level":"newbie",
     "languages": "html / css",
-    "deploy":"pages/3-column-preview-card-component/index.html",
-    "code":"",
+    "deploy":"pages/3-column-preview/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/3-column-preview",
   },
   {
     "image":"qr-code",
@@ -32,8 +32,8 @@ const projects = [
     "date":"09/08/2022",
     "level":"newbie",
     "languages": "html / css",
-    "deploy":"pages/qr-code-component/index.html",
-    "code":"",
+    "deploy":"pages/qr-code/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/qr-code",
   },
   {
     "image":"nft-equilibrium",
@@ -41,8 +41,8 @@ const projects = [
     "date":"07/08/2022",
     "level":"newbie",
     "languages": "html / css",
-    "deploy":"pages/nft-preview-card-component/index.html",
-    "code":"",
+    "deploy":"pages/nft-preview-card/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/nft-preview-card",
   },
   {
     "image":"perfume-gabrielle",
@@ -50,8 +50,8 @@ const projects = [
     "date":"06/08/2022",
     "level":"newbie",
     "languages": "html / css",
-    "deploy":"pages/product-preview-card-component-main/index.html",
-    "code":"",
+    "deploy":"pages/product-preview-card/index.html",
+    "code":"https://github.com/Vitor-HenriqueAS/Front_End_Mentor/tree/main/pages/product-preview-card",
   }
 ];
 
